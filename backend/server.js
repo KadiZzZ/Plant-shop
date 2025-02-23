61fb0b7d1a1c9f166038164ec532ba2ea612fcc8
KadiZzZ <kadizalala2@gmail.com>
2025-02-24 00:14:11 +0400

    v0.9
    pre-release version with docker

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors'); // Добавляем CORS
 const fs = require('fs');
+const path = require('path'); // Подключаем модуль path для работы с путями
 const app = express();
 
 app.use(cors()); // Разрешаем запросы с любого источника
@@ -11,10 +12,19 @@ app.post('/submit-order', (req, res) => {
     const { firstName, lastName, pickupDate } = req.body;
     const orderData = `Имя: ${firstName}, Фамилия: ${lastName}, Дата получения: ${pickupDate}\n`;
 
-    fs.appendFile('orders.txt', orderData, (err) => {
+    // Определяем путь к файлу
+    const filePath = path.join(__dirname, 'backend', 'orders.txt');
+
+
+    console.log('Путь к файлу:', filePath); // Логируем путь к файлу
+
+    // Записываем данные в файл
+    fs.appendFile(filePath, orderData, (err) => {
         if (err) {
             return res.status(500).json({ message: 'Ошибка при записи данных.' });
         }
+
+        console.log('Заказ сохранен:', orderData);
         res.json({ message: 'Данные успешно сохранены.' });
     });
 });
